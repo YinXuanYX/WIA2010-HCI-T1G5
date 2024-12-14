391ef09deff33ef49c48ba4fbf0202de0915ab56
ooijiahui <126597154+ooijiahui@users.noreply.github.com>
2024-12-14 20:23:45 +0800

    Added footer code

--- getrec_pg1.php
+++ getrec_pg1.php
@@ -4,6 +4,7 @@
 <head>
     <title>Get Recommendation pg1</title>
     <link rel="stylesheet" href="gc_style.css">
+    <link rel="stylesheet" href="css/hci.css">
 </head>
 
 <body>
@@ -20,23 +21,40 @@
         </div>
     </div>
 
-    <div class="white-section">
-        <h3 class="contact">CONTACT</h3>
-        <h3 class="phone"> Tel: +60 3-123 4567</h3>
-        <h3 class="email">Email: hcit1g5@gmail.com</h3>
 
-        <!-- Quicklinks Container (Aligned at bottom-right) -->
-        <div class="quicklinks-container">
-            <h3 class="quicklinks">QUICKLINKS</h3>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="">Get Recommendation</a></li>
-            </ul>
+
+    <!-- Footer Start -->
+    <div class="footer">
+        <div class="footer-content">
+            <div class="footer-section contact">
+                <p><strong>CONTACT</strong></p>
+                <p>Tel: +03-123 4567</p>
+                <p>Email: hciT15@gmail.com</p>
+                <div class="social-icons">
+                    <img src="img/ig-footer.png" alt="Instagram">
+                    <img src="img/linkedin-footer.png" alt="LinkedIn">
+                    <img src="img/facebook-footer.png" alt="Facebook">
+                    <img src="img/youtube-footer.png" alt="YouTube">
+                </div>
+            </div>
+
+            <div class="footer-section logo">
+                <img src="img/group-logo.png" alt="Logo" class="footer-logo" style="width:169px;height:62px;">
+                <p>TEACHING STRATEGY RECOMMENDATION WEBSITE</p>
+            </div>
+
+            <div class="footer-section quick-links">
+                <p><strong>QUICK LINKS</strong></p>
+                <ul>
+                    <li><a href="#">> Home</a></li>
+                    <li><a href="#">> About Us</a></li>
+                    <li><a href="#">> Contact</a></li>
+                    <li><a href="#">> Get Recommendation</a></li>
+                </ul>
+            </div>
         </div>
-
     </div>
+    <!-- Footer End -->
 
 </body>
 
