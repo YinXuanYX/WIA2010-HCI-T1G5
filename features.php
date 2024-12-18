a7ff3914d6f57c1cedf9b16385d75c6674b4865d
ooijiahui <126597154+ooijiahui@users.noreply.github.com>
2024-12-19 00:08:49 +0800

    Update features.php

--- features.php
+++ features.php
@@ -9,7 +9,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image with Text Boxes</title>
+    <title>Features</title>
     <link href="css/helper.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
     <!-- Bootstrap Libraries Stylesheet -->
